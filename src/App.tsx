65a52c1d543f3058fdf45a059e709135cba53c43
Ferransr <65186873+Ferransr@users.noreply.github.com>
2023-07-29 19:55:02 +0200

    ex1-Muestra nombre y modelo de nave.
    Se llama a la API mediante Axios dentro del componente starShips. No hay ningún estilo creado.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Starships from "./components/starShips.tsx";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Starships />
+    </div>
+  );
+};
+
+export default App;
